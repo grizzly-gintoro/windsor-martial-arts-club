d0d780e41a0e44619e6788066f7b9171608b3e08
Miguel Longid <145785482+grizzly-gintoro@users.noreply.github.com>
2024-07-30 13:42:28 -0400

    Add login-page.php

---
+++ login-page.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>WMAC | Home Page</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="./reset.css">
+        <link rel="stylesheet" href="./styles.css">
+    </head>
+</html>
