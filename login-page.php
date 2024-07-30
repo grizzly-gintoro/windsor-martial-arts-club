aabc43fbf5a0f57c255126af7deb8742e77c595b
Miguel Longid <145785482+grizzly-gintoro@users.noreply.github.com>
2024-07-30 13:55:59 -0400

    Edit login-page.php

--- login-page.php
+++ login-page.php
@@ -7,4 +7,20 @@
         <link rel="stylesheet" href="./reset.css">
         <link rel="stylesheet" href="./styles.css">
     </head>
+    <body>
+        <form action="login-script.php" method="post">
+            <h2>Log-in</h2>
+
+            <?php if(isset($_GET['error'])) { ?>
+                <p class="error"> <?php echo $_GET['error']; ?></p>
+            <?php } ?>
+
+            <label>Username</label>
+                <input type="text" name="username" placeholder="Username">
+            <label>Password</label>
+                <input type="password" name="password" placeholder="Password">
+            
+            <button type="submit">Login</button>
+        </form>
+    </body>
 </html>
